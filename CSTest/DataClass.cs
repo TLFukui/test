9022e7b0703ea026e88ace598f9fbd1217807df8
TLFukui <k_fukui@thinkilogic.co.jp>
2017-08-21 12:11:02 +0900

    CSVデータ読み込みと出力

---
+++ CSTest/DataClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTest
+{
+    class DataClass
+    {
+        Log log = new Log();
+        List<string[]> dataList = new List<string[]>();
+
+        public void ReadData()
+        {
+            try
+            {
+                string filePath = @"../../../data.csv";
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(filePath))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        dataList.Add(sr.ReadLine().Split(','));
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                log.ErrorLog(e.Message);
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public void WriteAllData()
+        {
+            log.DebugLog("全てのデータを表示します\n");
+            for (int i = 0; i < dataList.Count; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(dataList[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSTest/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTest
+{
+    class Log
+    {
+        [Conditional("DEBUG")]
+        public void DebugLog(string str)
+        {
+            Console.Write("[DBG]" + str);
+        }
+
+        public void ErrorLog(string str)
+        {
+            Console.Write("[ERR]" + str);
+        }
+    }
+}
